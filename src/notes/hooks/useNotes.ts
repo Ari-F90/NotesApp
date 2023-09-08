65555a390199dc0660dcb82027e4ab8868a64f28
Ariadna Frias <ariadna.812@gmail.com>
2023-09-08 20:46:12 +0200

    Update note model

---
+++ src/notes/hooks/useNotes.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useReducer } from "react";
+import { Note } from "../models/note";
+import { noteReducer } from "../reducer/notes.reducer";
+
+const initialState: Note[] = [];
+
+export const useNotes = () => {
+  const [notes, dispatch] = useReducer(noteReducer, initialState);
+
+  const notesTotal: number = notes.length;
+  const notesPending: number = notes.filter(
+    (note) => note.status === false
+  ).length;
+
+  const handleAdd = (note): void => {
+    const action = {
+      type: "add",
+      payload: note,
+    };
+    dispatch(action);
+  };
+
+  const handleUpdate = (id): void => {
+    const action = {
+      type: "update",
+      payload: id,
+    };
+    dispatch(action);
+  };
+
+  const handleDelete = (id): void => {
+    const action = {
+      type: "delete",
+      payload: id,
+    };
+    dispatch(action);
+  };
+  return { notes, notesTotal, handleAdd, handleUpdate, handleDelete };
+};
