b35335dfe489b6395c3fa4958a4427a07d07a2fb
Ariadna Frias <ariadna.812@gmail.com>
2023-09-09 14:10:19 +0200

    Update main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./core/app/App";
-import "./index.css";
+import "../src/index.scss";
+import { NotesProvider } from "./notes/context/notes.context.provider";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NotesProvider>
+      <App />
+    </NotesProvider>
   </React.StrictMode>
 );
