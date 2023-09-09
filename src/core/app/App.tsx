473fa8f6a3b0d081839a1841569bc05de88004ca
Ariadna Frias <ariadna.812@gmail.com>
2023-09-09 17:35:56 +0200

    Update app tsx

--- src/core/app/App.tsx
+++ src/core/app/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { CardList } from "../../components/cardList/cardList";
 import { Header } from "../../components/header/header";
+import { Form } from "../../components/form/Form";
 
 export const App = () => {
   return (
     <>
       <Header></Header>
       <CardList></CardList>
+      <Form />
     </>
   );
 };
