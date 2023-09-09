e841cc2d6f9bf584f0279267eccd6000b819bc02
Ariadna Frias <ariadna.812@gmail.com>
2023-09-09 14:10:44 +0200

    Add header and styles

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./header.module.scss";
+export const Header = () => {
+  return (
+    <>
+      <h1 className={styles.title}>Notes App</h1>
+    </>
+  );
+};
